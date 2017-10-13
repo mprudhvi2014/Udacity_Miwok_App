604432c347db4911bd3be55423e0423318583d30
Prudhvi Mandapati <mprudhvi2014@gmail.com>
2017-10-13 16:51:12 -0700

    Using Arraylist instead of Array

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -12,27 +14,27 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
         setTitle("Numbers");
 
-        String[] numbers = new  String[10];
-        numbers[0] = "One";
-        numbers[1] = "Two";
-        numbers[2] = "Three";
-        numbers[3] = "Four";
-        numbers[4] = "Five";
-        numbers[5] = "Six";
-        numbers[6] = "Seven";
-        numbers[7] = "Eight";
-        numbers[8] = "Nine";
-        numbers[9] = "Ten";
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
 
-        Log.v("Numbers Activity", "Number at word[0]:" + numbers[0]);
-        Log.v("Numbers Activity", "Number at word[1]:" + numbers[1]);
-        Log.v("Numbers Activity", "Number at word[2]:" + numbers[2]);
-        Log.v("Numbers Activity", "Number at word[3]:" + numbers[3]);
-        Log.v("Numbers Activity", "Number at word[4]:" + numbers[4]);
-        Log.v("Numbers Activity", "Number at word[5]:" + numbers[5]);
-        Log.v("Numbers Activity", "Number at word[6]:" + numbers[6]);
-        Log.v("Numbers Activity", "Number at word[7]:" + numbers[7]);
-        Log.v("Numbers Activity", "Number at word[8]:" + numbers[8]);
-        Log.v("Numbers Activity", "Number at word[9]:" + numbers[9]);
+        Log.v("Numbers Activity", "Number at word[0]:" + words.get(0));
+        Log.v("Numbers Activity", "Number at word[1]:" + words.get(1));
+        Log.v("Numbers Activity", "Number at word[2]:" + words.get(2));
+        Log.v("Numbers Activity", "Number at word[3]:" + words.get(3));
+        Log.v("Numbers Activity", "Number at word[4]:" + words.get(4));
+        Log.v("Numbers Activity", "Number at word[5]:" + words.get(5));
+        Log.v("Numbers Activity", "Number at word[6]:" + words.get(6));
+        Log.v("Numbers Activity", "Number at word[7]:" + words.get(7));
+        Log.v("Numbers Activity", "Number at word[8]:" + words.get(8));
+        Log.v("Numbers Activity", "Number at word[9]:" + words.get(9));
     }
 }
